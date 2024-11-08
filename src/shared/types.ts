0b31c66226abdc51373ece3879f04408d772d78d
LouisLanganay <louis.langanay@epitech.eu>
2024-11-08 23:47:32 +0100

    feat: project interfaces

---
+++ src/shared/types.ts
@@ -0,0 +1,24 @@
+interface Client {
+  host: string;
+}
+
+interface Server {
+  current_time: number;
+  services: {
+    name: string;
+    description: string;
+    actions: {
+      name: string;
+      description: string;
+    }[];
+    reactions: {
+      name: string;
+      description: string;
+    }[];
+  }[];
+}
+
+interface About {
+  client: Client;
+  server: Server;
+}
