7772717605494573ed44cef8694dbe734e9da1e4
justineloizel <justine.loizel@gmail.com>
2024-11-20 16:48:47 +0100

    fix(feat/admin-panel): eslint config file and translations

---
+++ src/web/eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+    },
+  },
+];
