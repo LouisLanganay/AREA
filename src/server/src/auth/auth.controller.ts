d5c0a4e6c8e73736b96b340b1f0b3bb766464b46
3W3N <ewen.emeraud@gmail.com>
2024-11-22 14:59:43 +0100

    feat(feat/init_api):add firt version of api root login, register and lost password. And a Jwt is created when login

---
+++ src/server/src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from '../users/dto/login-user.dto';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { ForgotPasswordDto } from '../users/dto/forgot-password.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('register')
+    async register(@Body() createUserDto: CreateUserDto) {
+        return this.authService.register(createUserDto);
+    }
+
+    @Post('login')
+    async login(@Body() loginUserDto: LoginUserDto) {
+        return this.authService.login(loginUserDto);
+    }
+
+    @Post('forgot-password')
+    async forgotPassword(@Body() forgotPasswordDto: ForgotPasswordDto) {
+        return this.authService.forgotPassword(forgotPasswordDto);
+    }
+}
---
+++ src/server/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    UsersModule, // Module pour la gestion des utilisateurs
+    PassportModule,
+    JwtModule.register({
+      secret: 'SECRET_KEY', // Remplacez par une clé plus sécurisée
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
