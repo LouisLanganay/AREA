aa97822eadcf45293cd455025ae32432bc37059a
tom <tom.lefoix@epitech.eu>
2024-11-25 18:43:43 +0100

    fix(feat/init_api): enable cors

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Active la validation pour toutes les routes
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
