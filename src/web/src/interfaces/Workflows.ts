f5a07c2da4abdda0366ea0c1bea6e6f019a64a70
LouisLanganay <louis.langanay@epitech.eu>
2024-11-26 20:40:45 +0100

    feat(web): update workflow display

---
+++ src/web/src/interfaces/Workflows.ts
@@ -0,0 +1,14 @@
+import { FieldGroup } from '../../../shared/Workflow';
+import { Service } from '../../../shared/Workflow';
+
+interface WorkflowNodeData {
+  label?: string;
+  status?: 'pending' | 'success' | 'error';
+  service?: Service;
+  fieldGroups?: FieldGroup[];
+  description?: string;
+  isTrigger?: boolean;
+  [key: string]: unknown;
+}
+
+export type { WorkflowNodeData };
