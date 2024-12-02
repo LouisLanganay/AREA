fc3b94be639e8cde09e8aef26f28fae09e691716
tom <tom.lefoix@epitech.eu>
2024-12-02 18:48:49 +0100

    feat(feat/db_workflows): add google authentication

--- src/server/src/auth/auth.controller.ts
+++ src/server/src/auth/auth.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from '../users/dto/login-user.dto';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { ForgotPasswordDto } from '../users/dto/forgot-password.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +31,20 @@ export class AuthController {
   @Post('forgot-password')
   async forgotPassword(@Body() forgotPasswordDto: ForgotPasswordDto) {
     return this.authService.forgotPassword(forgotPasswordDto);
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {
+    console.log('googleAuth: ', req);
+    // Cette méthode redirige vers Google pour l'authentification
+  }
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(@Req() req) {
+    // Une fois redirigé, cette méthode traite les informations de l'utilisateur
+
+    return this.authService.UserGoogle(req.user);
   }
 }
--- src/server/src/users/users.controller.ts
+++ src/server/src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  async getMe(@Req() req: any) {
+    return 'Hello World';
+  }
+}
