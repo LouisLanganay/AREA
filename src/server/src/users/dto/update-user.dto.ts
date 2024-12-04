dc5d4eff576997c1cac6ca2cd140c1dd402d686d
tom <tom.lefoix@epitech.eu>
2024-12-04 17:46:01 +0100

    feat(feat/add_update_delete_user): add delete and update user routes

--- src/server/src/users/dto/update-user.dto.ts
+++ src/server/src/users/dto/update-user.dto.ts
@@ -1,8 +1,29 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class updateUserDto {
-  @IsString()
-  id: string;
+  @IsOptional()
+  @IsEmail()
+  @ApiProperty({
+    name: 'email',
+    description: 'new email of user example: email@email.eu',
+  })
+  email?: string;
 
-  data: any;
+  @ApiProperty({
+    name: 'displayName',
+    description: 'New display name of user, example : Potiron32',
+  })
+  @IsOptional()
+  @IsString()
+  displayName?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUrl()
+  @ApiProperty({
+    name: 'avatarUrl',
+    description: 'New url of avatar',
+  })
+  avatarUrl?: string;
 }
--- src/server/src/users/users.controller.ts
+++ src/server/src/users/users.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import {
   ApiBearerAuth,
   ApiOperation,
   ApiTags,
   ApiResponse,
+  ApiBody,
 } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { UserDetailSuccess } from './dto/request.doc';
@@ -15,7 +25,7 @@ import { updateUserDto } from './dto/update-user.dto';
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @ApiBearerAuth() // Indique que cette route nécessite un JWT
+  @ApiBearerAuth()
   @ApiOperation({ summary: "Get the current user's information" }) // Résumé de l'opération
   @ApiResponse({
     status: 200,
@@ -33,10 +43,45 @@ export class UsersController {
   }
 
   @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @ApiOperation({ summary: "Update the current user's information" })
   @Post('update')
+  @ApiBody({
+    type: updateUserDto,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated successfully.',
+    schema: {
+      example: {
+        id: '123e4567-e89b-12d3-a456-426614174000',
+        email: 'updatedemail@example.com',
+        username: 'updatedUsername',
+        displayName: 'Updated Name',
+        avatarUrl: 'https://example.com/new-avatar.png',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 503,
+    description: 'database is unavailable or stop',
+  })
+  @HttpCode(200)
   @UseGuards(AuthGuard('jwt'))
-  async updateUser(@Req() req: updateUserDto) {
-    // Ajouter votre logique ici
+  async updateUser(@Req() req, @Body() body: updateUserDto) {
+    return await this.userService.updateUser(body, req.user.id);
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete a user connected' })
+  @ApiResponse({
+    status: 204,
+    description: 'The user has been successfully deleted.',
+  })
+  @HttpCode(204)
+  @UseGuards(AuthGuard('jwt'))
+  @Delete()
+  async deleteUser(@Req() req: any) {
+    await this.userService.deleteUser(req.user.id);
   }
 }
