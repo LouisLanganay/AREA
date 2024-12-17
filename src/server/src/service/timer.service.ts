8a463aaea12076091e904564288d102ef6550301
justineloizel <justine.loizel@gmail.com>
2024-12-17 14:17:39 +0100

    feat(timer-service): new service created with image

---
+++ src/server/src/service/timer.service.ts
@@ -0,0 +1,10 @@
+import {Service, Event, FieldGroup} from '../../../shared/Workflow';
+
+export const TimerService: Service = {
+    id: "timer",
+    name: "Timer Service",
+    description: "Service to handle timers",
+    loginRequired: false,
+    image: "https://www.svgrepo.com/show/532140/timer.svg",
+    Event: [],
+}
