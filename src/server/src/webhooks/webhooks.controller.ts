efd39bdad5d1b4d9141a1e593c62d1b57177628c
tom <tom.lefoix@epitech.eu>
2025-01-17 11:43:30 +0100

    doc(dev): doc for swagger

--- src/server/src/webhooks/webhooks.controller.ts
+++ src/server/src/webhooks/webhooks.controller.ts
@@ -1,11 +1,13 @@
 import { Req, Controller, Param, Post } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('webhooks')
 export class WebhooksController {
   constructor(private readonly webhooksService: WebhooksService) {}
 
   @Post(':id')
+  @ApiOperation({ summary: 'Handle webhook' })
   async handleWebhook(@Param('id') id: string, @Req() req: any) {
     return this.webhooksService.handleWebhook(id, req.headers);
   }
