1ded16767b07b3771d047e31a868c532e5b2939f
sebabacou <sebastien.bertilsouchet@protonmail.com>
2025-01-17 10:41:28 +0100

    hotfix(merge): missing UsersModule imports in workflow.module.ts

--- src/server/src/workflow/workflow.module.ts
+++ src/server/src/workflow/workflow.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { WorkflowService } from './workflow.service';
 import { WorkflowController } from './workflow.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   providers: [WorkflowService, PrismaService],
   controllers: [WorkflowController],
 })
