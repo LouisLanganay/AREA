36c2b1d6cc477d6ccc62bcc3973a3f237e7053af
3W3N <ewen.emeraud@gmail.com>
2025-01-19 11:26:21 +0100

    fix(dev): swagger description

--- src/server/src/webhooks/webhooks.controller.ts
+++ src/server/src/webhooks/webhooks.controller.ts
@@ -1,6 +1,6 @@
 import { Req, Controller, Param, Post, Get, Body, Res , Headers } from '@nestjs/common';
 import { WebhooksService } from './webhooks.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 
 @Controller('webhooks')
@@ -9,16 +9,21 @@ export class WebhooksController {
   }
 
   @Post(':id')
-  @ApiOperation({ summary: 'Handle webhook' })
+  @ApiOperation({ summary: 'Handle webhook with POST method' })
   async handleWebhook(@Param('id') id: string, @Req() req: any) {
     return this.webhooksService.handleWebhook(id, req.headers);
   }
 
   @Get(':id')
+    @ApiOperation({ summary: 'Handle webhook with GET method' })
   async handleWebhookGet(@Param('id') id: string, @Req() req: any) {
     return this.webhooksService.handleWebhook(id, req.headers);
   }
 
+  @ApiOperation({ summary: 'Handle Twitch webhook' })
+  @ApiBody({ type: Object })
+  @ApiResponse({ status: 200, description: 'OK' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
   @Post('twitch/:webhookId')
   async handleTwitchWebhook(
       @Param('webhookId') webhookId: string,
